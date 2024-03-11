4d2505a7f6ed4c618f6078259737e4f16ae48508
Taxato <taxatoftw@gmail.com>
2024-03-11 15:31:49 +0100

    Fixed background image

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -2,7 +2,7 @@ export default function Background() {
 	return (
 		<img
 			src="imgs/fire.jpg"
-			className="fixed -top-2 left-8 -z-50 scale-[2] opacity-[35%] sm:-top-[70%] sm:scale-75"
+			className="fixed bottom-[40%] left-8 -z-50 scale-[2] opacity-[35%] md:bottom-[10%] lg:-bottom-[20%] lg:top-auto lg:scale-100 xl:-bottom-[50%]"
 		/>
 	);
 }
