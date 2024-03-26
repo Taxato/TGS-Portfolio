b036f73fc9ef9d2e9c9e54c156584b1f103eb72e
Taxato <taxatoftw@gmail.com>
2024-03-26 14:29:54 +0100

    mobile stretch bug fix

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,10 +1,7 @@
 export default function Icon({ name, children }) {
 	return (
 		<div className="flex flex-col items-center gap-1">
-			<img
-				src={`imgs/${name}`}
-				className="h-10 w-fit rounded-lg sm:h-16"
-			/>
+			<img src={`imgs/${name}`} className="h-10 rounded-lg sm:h-16" />
 			{children}
 		</div>
 	);
