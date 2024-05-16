d64c8f40c8900a3e997f5112377a030a2c446e9c
Thomas <taxatoftw@gmail.com>
2024-05-16 13:53:45 +0200

    Update Background.jsx

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,7 +3,8 @@ export default function Background() {
 		<div className="fixed left-0 top-0 -z-50 flex h-screen w-screen items-center justify-center overflow-hidden">
 			<img
 				src="imgs/fire.jpg"
-				className="min-h-full min-w-full object-cover opacity-35"
+				className="min-h-full min-w-full object-cover opacity-0 transition-opacity duration-1000"
+				onLoad={e => (e.target.style.opacity = 0.35)}
 			/>
 		</div>
 	);
